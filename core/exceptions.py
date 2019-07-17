60912dddda94b76afa90f687eeeee2a6708358f3
Shchepetov Danil <panaev2853@gmail.com>
2019-07-18 00:58:32 +0300

    Improved IncorrectCommand exception

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,9 +1,13 @@
 class IncorrectCommand(Exception):
 
-    def __init__(self, command=None, **extra_kwargs):
+    def __init__(self, message, command=None, **extra_kwargs):
+        self.message = message
         self.command = command
         self.extra_data = extra_kwargs
         super().__init__(command, extra_kwargs)
 
     def __str__(self):
-        return 'Incorrect command: {}'.format(self.command)
+        message = self.message
+        if self.command:
+            message += ' Команда "{}"'.format(self.command)
+        return message
